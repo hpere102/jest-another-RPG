c3f03d572685c716d8757c82c9566348cb64e9d7
danny p <danny@dannys-MacBook-Pro.local>
2021-11-15 23:48:11 -0500

    player added

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -4,6 +4,7 @@ console.log(new Potion());
 
 const Player = require('../lib/Player');
 
+// test 1
 test('creates a player object', () => {
     const player = new Player('Dave');
   
@@ -15,4 +16,26 @@ test('creates a player object', () => {
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
       );
+
+  });
+
+// test 2
+      test("gets player's stats as an object", () => {
+        const player = new Player('Dave');
+      
+        expect(player.getStats()).toHaveProperty('potions');
+        expect(player.getStats()).toHaveProperty('health');
+        expect(player.getStats()).toHaveProperty('strength');
+        expect(player.getStats()).toHaveProperty('agility');
+      });
+
+  // test 3      
+  test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+  
+    expect(player.getInventory()).toEqual(expect.any(Array));
+  
+    player.inventory = [];
+  
+    expect(player.getInventory()).toEqual(false);
   });
